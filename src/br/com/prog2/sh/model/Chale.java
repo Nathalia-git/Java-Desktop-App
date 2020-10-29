a1f11e7e008da909ca618655e7e63d81d29e7874
Nathalia-git <57800589+Nathalia-git@users.noreply.github.com>
2020-10-29 12:23:54 -0300

    Create Chale.java

---
+++ src/br/com/prog2/sh/model/Chale.java
@@ -0,0 +1,44 @@
+package br.com.prog2.sh.model;
+
+public class Chale {
+	private String codChale;
+	private String localizacao;
+	private Integer capacidade;
+	private Double valorAltaEstacao;
+	private Double valorBaixaEstacao;
+	
+	public Chale() {}
+	
+	public String getCodChale() {
+		return codChale;
+	}
+	public void setCodChale(String codChale) {
+		this.codChale = codChale;
+	}
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	public Integer getCapacidade() {
+		return capacidade;
+	}
+	public void setCapacidade(Integer capacidade) {
+		this.capacidade = capacidade;
+	}
+	public Double getValorAltaEstacao() {
+		return valorAltaEstacao;
+	}
+	public void setValorAltaEstacao(Double valorAltaEstacao) {
+		this.valorAltaEstacao = valorAltaEstacao;
+	}
+	public Double getValorBaixaEstacao() {
+		return valorBaixaEstacao;
+	}
+	public void setValorBaixaEstacao(Double valorBaixaEstacao) {
+		this.valorBaixaEstacao = valorBaixaEstacao;
+	}
+	
+	
+}
