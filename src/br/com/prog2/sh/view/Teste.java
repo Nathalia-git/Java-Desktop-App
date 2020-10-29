74870d8e5ee448545556ab6a0a50f095250d63ff
Nathalia-git <57800589+Nathalia-git@users.noreply.github.com>
2020-10-29 15:02:21 -0300

    Create Teste.java

---
+++ src/br/com/prog2/sh/view/Teste.java
@@ -0,0 +1,140 @@
+package br.com.prog2.sh.view;
+
+
+public class Teste {
+	public static void main(String[] args) {
+//		Connection con = ConnectionFactory.getConnection();
+//		if (con != null) {
+//			System.out.println("OK");
+//			ConnectionFactory.close(con);
+//		} else {
+//			System.out.println("Erro");
+//		}
+
+//		cente c1 = new cente();
+//		centeController cc = new centeController();
+//		c1.setCodcente("444");
+//		c1.setNomeCliente("Mariana");
+//		c1.setRgCliente("32189");
+//		c1.setBairroCliente("Rua das Palmeiras");
+//		c1.setCidadeCliente("Samamabaia");
+//		c1.setEstadoCliente("DF");
+//		c1.setCEPCliente("7241356");
+//		c1.setNascimentoCliente(LocalDate.of(1999, 07, 02));
+//		System.out.println(cc.inserir(c1));
+//		
+//		cente c2 = new cente();
+//		c2.setCodcente("555");
+//		c2.setNomeCliente("Ronaldo");
+//		c2.setRgCliente("213456");
+//		c2.setBairroCliente("Rua das Avenidas");
+//		c2.setCidadeCliente("Taguatinga");
+//		c2.setEstado("DF");
+//		c2.setCep("51651890");
+//		c2.setNascimentoCliente(LocalDate.of(2002, 02, 02));
+//		System.out.println(cc.inserir(c2));
+
+//		cente c1 = new cente();
+//		centeController cc = new centeController();
+//		c1.setCodcente("666");
+//		c1.setNomeCliente("Nathalia");
+//		c1.setRgCliente("32150321");
+//		c1.setBairroCliente("Avenida Singular");
+//		c1.setCidadeCliente("Guara");
+//		c1.setEstadoCliente("DF");
+//		c1.setCEPCliente("23198625");
+//		c1.setNascimentoCliente(LocalDate.of(2001, 08, 13));
+//		System.out.println(cc.alterar(c1));
+
+//		cente c3 = new cente();
+//		centeController cc = new centeController();
+//		c3.setCodcente("555");
+//		System.out.println(cc.excluir(c3));
+
+//		List<cente> lista = new ArrayList<cente>();
+//		centeController cc = new centeController();
+//		lista = cc.listarTodos();
+//		if (lista != null) {
+//			for (cente c : lista) {
+//				System.out.println("cente: " + c.getCodcente());
+//				System.out.println("Nome: " + c.getNomeCliente());
+//			}
+//		}
+
+//		ClienteController controller = new ClienteController();
+//		Cliente c = new Cliente();
+//		c = controller.pesquisarPorCliente("666");
+//		if (c != null) {
+//			System.out.println("Código do cente: " + c.getCodCliente());
+//			System.out.println("Nome: " + c.getNomeCliente());
+//			System.out.println("RG: " + c.getRgCliente());
+//			System.out.println("Bairro: " + c.getBairroCliente());
+//			System.out.println("Cidade: " + c.getCidadeCliente());
+//			System.out.println("Estado: " + c.getEstadoCliente());
+//			System.out.println("CEP: " + c.getCEPCliente());
+//			System.out.println("Data de Nascimento: " + c.getNascimentoCliente());
+//		}
+		
+//		Chale c = new Chale();
+//		ChaleController cc = new ChaleController();
+//		c.setCodChale("12");
+//		c.setLocalizacao("Caldas Novas");
+//		c.setCapacidade(8);
+//		c.setValorAltaEstacao(180.00);
+//		c.setValorBaixaEstacao(100.00);
+//		System.out.println(cc.inserir(c));
+		
+//		Chale c = new Chale();
+//		ChaleController cc = new ChaleController();
+//		c.setCodChale("01");
+//		c.setLocalizacao("Caldas Novas");
+//		c.setCapacidade(4);
+//		c.setValorAltaEstacao(100.00);
+//		c.setValorBaixaEstacao(70.00);
+//		System.out.println(cc.alterar(c));
+		
+//		Chale c = new Chale();
+//		ChaleController cc = new ChaleController();
+//		c.setCodChale("01");
+//		System.out.println(cc.excluir(c));
+		
+//		List<Chale> lista = new ArrayList<>();
+//		ChaleController cc = new ChaleController();
+//		lista = cc.listarTodos();
+//		if (lista != null) {
+//			for (Chale ch : lista) {
+//				System.out.println("Codigo do Cliente: " + ch.getCodChale());
+//				System.out.println("Localizacao: " + ch.getLocalizacao());
+//				System.out.println("Capacidade: " + ch.getCapacidade());
+//				System.out.println("Valor da Alta Estação: " + ch.getValorAltaEstacao());
+//				System.out.println("Valor da Baixa Estação: " + ch.getValorBaixaEstacao());
+//			}
+//		}
+		
+//		Chale c = new Chale();
+//		ChaleController cc = new ChaleController();
+//		c = cc.pesquisarPorCod("12");
+//		if (c != null) {
+//			System.out.println("Codigo do Cliente: " + c.getCodChale());
+//			System.out.println("Localizacao: " + c.getLocalizacao());
+//			System.out.println("Capacidade: " + c.getCapacidade());
+//			System.out.println("Valor da Alta Estação: " + c.getValorAltaEstacao());
+//			System.out.println("Valor da Baixa Estação: " + c.getValorBaixaEstacao());
+//		}
+//		List<Hospedagem> listaHosp = new ArrayList<>();
+//		HospedagemController hc = new HospedagemController();
+//		listaHosp = hc.listarTodos();
+//		for (Hospedagem h : listaHosp) {
+//			System.out.println("Codigo: " + h.getCodChale());
+//			h.setValorFinal(h.calculaValor(h.getValorFinal(), h.getDesconto()));
+//			System.out.println("Valor: " + h.getValorFinal());
+//		}
+		
+//		Hospedagem h = new Hospedagem();
+//		HospedagemController hc = new HospedagemController();
+//		System.out.println(hc.alterar(h));
+		System.out.println("teste");
+	}
+}
+
+
