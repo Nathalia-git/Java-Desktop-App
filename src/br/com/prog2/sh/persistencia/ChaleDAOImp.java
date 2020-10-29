2d3ac81ec06e49578b1fb254821a4778b1874d97
Nathalia-git <57800589+Nathalia-git@users.noreply.github.com>
2020-10-29 12:38:27 -0300

    Create ChaleDAOImp.java

---
+++ src/br/com/prog2/sh/persistencia/ChaleDAOImp.java
@@ -0,0 +1,39 @@
+package br.com.prog2.sh.persistencia;
+
+import java.util.List;
+
+import br.com.prog2.sh.model.Chale;
+
+public class ChaleDAOImp implements ChaleDAO {
+
+	@Override
+	public String inserir(Chale ch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String alterar(Chale ch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String excluir(Chale ch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Chale> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Chale pesquisarPorCod(String codChale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
